4b4ce086d57f8d38d48113a6c48bd981c7a01004
Adarsh-oo7 <adarshsarachandran@gmail.com>
2024-06-15 20:54:31 +0530

    base complected

--- events/userapp/views.py
+++ events/userapp/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib import auth
 # Create your views here.
+def reg(request):
+    if request.POST:
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        cpassword=request.POST.get('cpassword')
+        if password==cpassword:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'this user name is already taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'this email is already taken')
+                return redirect('register')
+            else:
+                user_reg=User.objects.create_user(username=username,password=password,email=email)
+                user_reg.save()
+                messages.info(request,'Registed Successfully')
+                return redirect('/')
+        else :
+            messages.info(request,'Plese check Your password')
+            return redirect('register')
+
+
+
+    return render(request,'reg.html')
+
+def login(request):
+    if request.POST:
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            messages.info(request,'login Success')
+            return redirect('/')
+        else:
+            messages.info(request,'check the username and password')
+            return redirect('login')
+    return render(request,'log.html')
